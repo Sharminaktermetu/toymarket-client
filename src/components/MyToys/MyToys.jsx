c198a460b9c668b75a3c0dbd7c2fafd8cf75bb5c
Sharminaktermetu <sharminacc1@gmail.com>
2023-06-08 21:31:11 +0600

    sweet alert set

--- src/components/MyToys/MyToys.jsx
+++ src/components/MyToys/MyToys.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
 import MyToysRow from './MyToysRow';
+import Swal from 'sweetalert2';
 
 const MyToys = () => {
     const {user}=useContext(AuthContext)
@@ -16,25 +17,37 @@ const MyToys = () => {
             })
         },[])
 
-        const handleDelete=(id)=>{
-            const procedd =confirm('Are you sure?')
-            if (procedd) {
-                fetch(`http://localhost:5000/toy/${id}`,{
-                method:'DELETE',
-                
-            })
-            .then(res=>res.json())
-            .then(data=>{
-                console.log(data);
-                if(data.deletedCount>0){
-                    alert('Service deleted')
-                    const remaining =toys.filter(toy=>toy._id !== id)
-                    setToys(remaining)
-                  }
-            })
-            }
-            
-        }
+        const handleDelete = (id) => {
+            Swal.fire({
+              title: 'Are you sure?',
+              text: "You won't be able to revert this!",
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'Yes, delete it!',
+            }).then((result) => {
+              if (result.isConfirmed) {
+                fetch(`http://localhost:5000/toy/${id}`, {
+                  method: 'DELETE',
+                })
+                  .then((res) => res.json())
+                  .then((data) => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                      Swal.fire(
+                        'Deleted!',
+                        'Your toy has been deleted.',
+                        'success'
+                      );
+                      const remaining = toys.filter((toy) => toy._id !== id);
+                      setToys(remaining);
+                    }
+                  });
+              }
+            });
+          };
+          
 
     return (
         <div>
