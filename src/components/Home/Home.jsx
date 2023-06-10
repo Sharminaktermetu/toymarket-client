e408a64440d767bb07b8bb263d2ebc712c43b83d
Sharminaktermetu <sharminacc1@gmail.com>
2023-06-10 12:33:34 +0600

    environment setup

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from './Banner';
 import { Tab, Tabs } from 'react-tabs';
 
@@ -6,16 +6,24 @@ import GallerySection from './GallerySection';
 import MyTabsComponent from './Tabs/MyTabsComponent ';
 import Brands from './Brands';
 import Steps from './Steps';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import AOS CSS file
 
 const Home = () => {
+    useEffect(()=>{
+        AOS.init({duration:3000});
+    },[])
+
     return (
         <>
        
             <Banner></Banner>
-            <GallerySection></GallerySection>
-            <MyTabsComponent></MyTabsComponent>
-            <Brands></Brands>
-            <Steps></Steps>
+            <div data-aos="fade-down"><GallerySection></GallerySection></div>
+            <div data-aos="fade-right"><MyTabsComponent></MyTabsComponent></div>
+            <div data-aos="fade-left"> <Brands></Brands></div>
+            <div data-aos="fade-down"> <Steps></Steps></div>
+            
+
         
          </>
     );
--- src/firebase/firebse.config.js
+++ src/firebase/firebse.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBqPlcn6L1iXHyGJiFXWbyQypo7hdlWlQo",
-  authDomain: "tiny-toy-town.firebaseapp.com",
-  projectId: "tiny-toy-town",
-  storageBucket: "tiny-toy-town.appspot.com",
-  messagingSenderId: "644307942461",
-  appId: "1:644307942461:web:2f1ba73997db963dbfbe54"
+apiKey:import.meta.env.VITE_apiKey,
+authDomain:import.meta.env.VITE_authDomain,
+projectId:import.meta.env.VITE_projectId,
+storageBucket:import.meta.env.VITE_storageBucket,
+messagingSenderId:import.meta.env.VITE_messagingSenderId,
+appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
